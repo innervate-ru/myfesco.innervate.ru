da01e790ffb7882cd1737df41a8cc8b37c305fac
Markov Alexey <mark@delightsoft.ru>
2018-12-12 14:10:43 +0300

    обновил ссылки для https

--- static/js/like-counter.js
+++ static/js/like-counter.js
@@ -1,5 +1,5 @@
 const axiosInstance = axios.create({
-    baseURL: 'http://85.143.173.184:3066/',
+    baseURL: 'https://omua.edubot.pro/',
     responseType: 'json',
     headers: {
         'Accept': 'application/json',
@@ -17,13 +17,14 @@ $(document).ready(function () {
             if (response.data.ok) {
                 // прописываем значение счетчика на кнопке
                 $(".is-helpful_yes_count").text(response.data.result.like)
+                $(".is-helpful_total_count").text(+response.data.result.like + +response.data.result.dislike)
             }
         })
         .catch(function (error) {
             console.log(error);
         })
 
-    // обработка нового клика
+    // обработка нового клика 'like'
     $(".is-helpful__btn.button-y").click(function () {
         axiosInstance.post( '/newClick', {
             url: window.location.pathname,
@@ -33,6 +34,25 @@ $(document).ready(function () {
                 if (response.data.ok) {
                     // прописываем значение счетчика на кнопке
                     $(".is-helpful_yes_count").text(response.data.result.like)
+                    $(".is-helpful_total_count").text(+response.data.result.like + +response.data.result.dislike)
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    });
+
+    // обработка нового клика 'dislike'
+    $(".is-helpful__btn.button-n").click(function () {
+        axiosInstance.post( '/newClick', {
+            url: window.location.pathname,
+            type: 'dislike'
+        })
+            .then(function (response) {
+                if (response.data.ok) {
+                    // прописываем значение счетчика на кнопке
+                    $(".is-helpful_yes_count").text(response.data.result.like)
+                    $(".is-helpful_total_count").text(+response.data.result.like + +response.data.result.dislike)
                 }
             })
             .catch(function (error) {
