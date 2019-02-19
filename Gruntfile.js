17d4f4c91ac3c5638809f40721e577226883487b
Markov Alexey <mark@delightsoft.ru>
2019-02-19 19:23:37 +0000

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
                 title: content.data.title,
                 categories: content.data.categories,
                 tags: content.data.tags,
-                href: '/help/' + href,
+                href: ('/help/' + href).replace("//", "/"),
                 content: S(modifiedContent).trim().stripTags().stripPunctuation().s
             };
         };
