bc84d8c96e92f843dbf4bf21121b388f43763811
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2019-03-25 13:38:47 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -33,12 +33,6 @@ git_pull(){
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-cd public
-git fetch --all
-git reset --hard origin/master
-git pull
-cd ..
-
 # обновление из git основного проекта
 echo -e "\033[0;32m dev git pull...\033[0m"
 git_pull
@@ -50,12 +44,9 @@ if is_err; then return; fi
 grunt search-index
 
 # Build the project.
-hugo
+hugo -d ./docs
 
-cd public
 # отправляем сгенерированный проект в удаленный репозиторий
 echo -e "\033[0;32m dev git push...\033[0m"
 git_push
 if is_err; then return; fi
-
-cd ..
